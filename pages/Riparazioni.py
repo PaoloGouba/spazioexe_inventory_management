b9ab218d83eb31e9f9b0d466560dbdf8ba238b60
PaoloGouba <pfgouba@gmail.com>
2024-05-07 11:30:29 +0200

    Update Riparazioni.py

--- pages/Riparazioni.py
+++ pages/Riparazioni.py
@@ -7,6 +7,9 @@ from modules.models import Reparation
 from utils.tools import get_data, gen_recap
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = fetch_reparations(reparations_worksheet)
 
 st.set_page_config(
     page_icon="🧊",
@@ -61,28 +64,82 @@ st.header('Riparazioni')
 
 col_1, col_2 = st.columns(2)
 
-with col_1: 
-    with st.expander(label="Visualizza riparazioni") :
-        df = fetch_reparations(reparations_worksheet)
-        selected_names = st.multiselect('Filtra per nome:', options=df['first_name'].unique())
-        selected_last_names = st.multiselect('Filtra per cognome:', options=df['last_name'].unique())
-        selected_devices = st.multiselect('Filtra per tipo di dispositivo:', options=df['device'].unique())
-        selected_brand = st.multiselect('Filtra per Marca:', options=df['brand'].unique())
-        selected_state = st.multiselect('Filtra per stato:', options=df['state'].unique())
-        selected_action = st.multiselect('Filtra per azione:', options=df['action'].unique())
-        
+with col_2: 
 
-        filtered_df = df[
-            (df['first_name'].isin(selected_names) if selected_names else df['first_name'].notnull()) &
-            (df['last_name'].isin(selected_last_names) if selected_last_names else df['last_name'].notnull()) &
-            (df['device'].isin(selected_devices) if selected_devices else df['device'].notnull()) &
-            (df['brand'].isin(selected_brand) if selected_brand else df['brand'].notnull()) & 
-            (df['state'].isin(selected_state) if selected_state else df['state'].notnull()) & 
-            (df['action'].isin(selected_action) if selected_action else df['action'].notnull())
-        ]
-        st.caption('Lista riparazioni')
-        # Mostra il DataFrame filtrato
-        st.dataframe(filtered_df)
+    with st.expander("Statistiche riparazioni", expanded=True) :
+
+        insight_type = ("brand","")
+
+        ## INSIGHTS RIPARAZIONI
+
+        if 'price' in df.columns and 'brand' in df.columns and 'state' in df.columns and not df.empty:
+            st.subheader("Totale riparazioni : "+ str(len(df)), divider=True)
+            #average_price = df['price'].mean().round(2)
+            #average_price_str = f"€{average_price:,.2f}"
+            #print("Average Price of Devices:", average_price_str)
+
+            orders_by_state = df['state'].value_counts()
+            st.subheader("Riparazioni per stato:")
+            # Grafico a barre per la distribuzione degli ordini per stato
+            plt.figure(figsize=(10, 6))
+            sns.barplot(x=orders_by_state.index, y=orders_by_state.values)
+            plt.xlabel('Stato')
+            plt.ylabel('Numero Ordini')
+            plt.title('Distribuzione degli ordini per stato')
+            plt.xticks(rotation=45)
+            st.pyplot(plt)  # Usa st.pyplot() invece di plt.show()
+            for state, count in orders_by_state.items():
+                st.write(f" - {state}: {count} items")
+
+
+
+            popular_brands = df['brand'].value_counts().head(5)
+            st.subheader("I 5 Brand più riparati:")
+            # Grafico a torta per i marchi più popolari
+            plt.figure(figsize=(8, 8))
+            plt.pie(popular_brands.values, labels=popular_brands.index, autopct='%1.1f%%')
+            plt.title('Top 5 Popular Brands')
+            st.pyplot(plt)  # Usa st.pyplot() invece di plt.show()
+            i=1
+            for brand, count in popular_brands.items():
+                st.write(f"{str(i)}. {brand}: {count} items")
+                i+=1
+
+
+
+            
+
+
+
+        ## OLD TEST INSIGHT
+        #st.subheader("Deprecated")
+
+
+        # Controllo delle colonne necessarie
+        #if 'device' in df.columns and 'state' in df.columns:
+            # Selezione dello stato da filtrare
+        #    unique_status = df['state'].unique()
+        #    selected_status = st.multiselect('Seleziona Stato/i', unique_status, default=unique_status)
+
+            # Filtraggio dei dati basato sullo stato selezionato
+        #    insight_filtered_df = df[df['state'].isin(selected_status)]
+
+            # Creazione dell'istogramma
+        #    fig, ax = plt.subplots()
+        #    insight_filtered_df['device'].value_counts().plot(kind='bar', ax=ax, color='skyblue')
+        #    ax.set_xlabel('Tipo di Dispositivo')
+        #    ax.set_ylabel('Frequenza')
+        #    ax.set_title('Distribuzione dei Tipi di Dispositivo per Stato Selezionato')
+        #    plt.xticks(rotation=45)  # Ruota le etichette degli assi x per una migliore leggibilità
+
+            # Mostra il grafico in Streamlit
+        #    st.pyplot(fig)
+        #else:
+        #    st.error('Il DataFrame non contiene le colonne "device" o "status". Verifica i dati.')
+
+
+
+
 
 
     with st.expander("Nuova riparazione") :
@@ -164,7 +221,7 @@ with col_1:
                     st.error(f"Errore di validazione: {str(e)}")
 
                 # Ricarica i dati e aggiorna la visualizzazione
-                df = get_data()  # Ricarica i dati
+                df = get_data(reparations_worksheet)  # Ricarica i dati
                 st.write(df)     # Aggiorna la visualizzazione
 
 
@@ -176,48 +233,30 @@ with col_1:
             #display_reparations()  # Aggiorna la visualizzazione dei dati
 
 
-    with st.expander("Insights riparazioni", expanded=True) :
-  
-        if 'device' in df.columns:
-            # Creazione del grafico a torta
-            fig, ax = plt.subplots()
-            df['device'].value_counts().plot(kind='pie', autopct='%1.1f%%', startangle=90, ax=ax)
-            ax.set_ylabel('')  # Rimuove l'etichetta dell'asse y
-            ax.set_title('Distribuzione dei Tipi di Dispositivo')
-            # Mostra il grafico in Streamlit
-            st.pyplot(fig)
-        else:
-            st.error('La colonna "device" non è presente nel DataFrame. Verifica i dati.')
 
 
-        # Controllo delle colonne necessarie
-        if 'device' in df.columns and 'state' in df.columns:
-            # Selezione dello stato da filtrare
-            unique_status = df['state'].unique()
-            selected_status = st.multiselect('Seleziona Stato/i', unique_status, default=unique_status)
-
-            # Filtraggio dei dati basato sullo stato selezionato
-            insight_filtered_df = df[df['state'].isin(selected_status)]
-
-            # Creazione dell'istogramma
-            fig, ax = plt.subplots()
-            insight_filtered_df['device'].value_counts().plot(kind='bar', ax=ax, color='skyblue')
-            ax.set_xlabel('Tipo di Dispositivo')
-            ax.set_ylabel('Frequenza')
-            ax.set_title('Distribuzione dei Tipi di Dispositivo per Stato Selezionato')
-            plt.xticks(rotation=45)  # Ruota le etichette degli assi x per una migliore leggibilità
-
-            # Mostra il grafico in Streamlit
-            st.pyplot(fig)
-        else:
-            st.error('Il DataFrame non contiene le colonne "device" o "status". Verifica i dati.')
-
+with col_1:
+    with st.expander(label="Visualizza riparazioni") :
+        
+        selected_names = st.multiselect('Filtra per nome:', options=df['first_name'].unique())
+        selected_last_names = st.multiselect('Filtra per cognome:', options=df['last_name'].unique())
+        selected_devices = st.multiselect('Filtra per tipo di dispositivo:', options=df['device'].unique())
+        selected_brand = st.multiselect('Filtra per Marca:', options=df['brand'].unique())
+        selected_state = st.multiselect('Filtra per stato:', options=df['state'].unique())
+        selected_action = st.multiselect('Filtra per azione:', options=df['action'].unique())
         
 
-
-
-
-with col_2:
+        filtered_df = df[
+            (df['first_name'].isin(selected_names) if selected_names else df['first_name'].notnull()) &
+            (df['last_name'].isin(selected_last_names) if selected_last_names else df['last_name'].notnull()) &
+            (df['device'].isin(selected_devices) if selected_devices else df['device'].notnull()) &
+            (df['brand'].isin(selected_brand) if selected_brand else df['brand'].notnull()) & 
+            (df['state'].isin(selected_state) if selected_state else df['state'].notnull()) & 
+            (df['action'].isin(selected_action) if selected_action else df['action'].notnull())
+        ]
+        st.caption('Lista riparazioni')
+        # Mostra il DataFrame filtrato
+        st.dataframe(filtered_df)
     try :
         i = 0
         for row in filtered_df.itertuples():
@@ -241,7 +280,22 @@ with col_2:
 
             c_text : str = "Cliente : " + first_name + " " + last_name + "\n" + "Azione : " + action
 
-            card(title=device + " - " + brand + " - " + state, text=c_text)
+            card(
+                title=device + " - " + brand + " - " + state,
+                text=c_text,
+                styles={
+                        "card": {
+                            "background" : "black",
+                            #"width": "500px",
+                            #"height": "500px",
+                            #"border-radius": "60px",
+                            #"box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+                                },
+                        "text": {
+                            #"font-family": "serif",
+                                }
+                        }
+                )
 
             download_input_data = {
                 "request_date" : request_date,
@@ -296,9 +350,8 @@ with col_2:
             if i == 1 :
                 break
             
-
     except :
-        raise
+        #raise
         st.info("Scegli la riparazione da consultare") 
 
 
